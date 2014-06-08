58c680586467ecc9843526e7d6edcc31de910caf
Katerina Marazopoulou <kmarazo@cs.umass.edu>
2014-06-08 18:47:34 -0400

    Script to preprocess data

---
+++ cleanData.R
@@ -0,0 +1,25 @@
+# read in data
+data <- read.table("household_power_consumption.txt", sep=";", header=T)
+backup.original <- data
+
+# create new column for a string date+time
+dateTimes <- paste(data$Date, data$Time)
+# convert to time
+dateTimes <- strptime(dateTimes, format = "%d/%m/%Y %H:%M:%S")
+# add it to the data frame
+data <- cbind(data, dateTimes)
+
+# convert columns to appropriate types
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+data$Global_active_power <- as.numeric(as.character(data$Global_active_power))
+data$Sub_metering_1 <- as.numeric(as.character(data$Sub_metering_1))
+data$Sub_metering_2 <- as.numeric(as.character(data$Sub_metering_2))
+data$Sub_metering_3 <- as.numeric(as.character(data$Sub_metering_3))
+
+# subset data for dates between 2007-02-01 and 2007-02-02
+date1 <- as.Date("2007-02-01")
+date2 <- as.Date("2007-02-02")
+data <- subset(data, data$Date<=date2 & data$Date>=date1)
+
+# keep around a copy of data
+data.backup <- data
